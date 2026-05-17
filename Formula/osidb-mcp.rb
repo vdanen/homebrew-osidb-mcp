6a09a23c03365e6143503247841ef7c0f0358e04
Vincent Danen <vdanen@redhat.com>
2026-05-17 07:10:40 -0600

    first commit

---
+++ Formula/osidb-mcp.rb
@@ -0,0 +1,25 @@
+class OsidbMcp < Formula
+  desc "Model Context Protocol (MCP) server for OSIDB"
+  homepage "https://github.com/vdanen/osidb-mcp"
+  url "https://files.pythonhosted.org/packages/source/o/osidb-mcp/osidb-mcp-0.2.0.tar.gz"
+  sha256 "829257b00139f4b0e40901234dcd13e2c6226624c1bc171d87d7cf9b912f883b"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  skip_clean "libexec"
+
+  def install
+    python3 = Formula["python@3.13"].opt_bin/"python3.13"
+    system python3, "-m", "venv", "--clear", libexec
+    ENV.delete("PIP_NO_BINARY")
+    ENV.delete("PIP_REQUIRE_HASHES")
+    system libexec/"bin/pip", "install", "--upgrade", "pip"
+    system libexec/"bin/pip", "install", "."
+    bin.install_symlink libexec/"bin/osidb-mcp"
+  end
+
+  test do
+    assert_match %r{\A\d+\.\d+\.\d+\n\z}, shell_output("#{bin}/osidb-mcp --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,26 @@
+# Homebrew Tap: osidb-mcp
+
+Homebrew formula for [osidb-mcp](https://github.com/vdanen/osidb-mcp) - MCP server to interact with OSIDB. LLMs can interface with flaws and records via MCP and natural language commands.
+
+## Installation
+
+```sh
+brew tap vdanen/osidb-mcp
+brew install osidb-mcp
+```
+
+Refer to the manpages or the
+[osidb-mcp](https://github.com/vdanen/osidb-mcp) git repo for more
+information on uage.
+
+## Updating
+
+```sh
+brew update
+brew upgrade osidb-mcp
+```
+
+## Supported Platforms
+
+* any that osidb-bindings supports
+
